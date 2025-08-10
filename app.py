62370994f7e389ca10a7e0b237c156472956b29e
sauravkumar0002 <sauravkumar9102897070@gmail.com>
2025-08-10 13:28:42 +0530

    new change in files

--- app.py
+++ app.py
@@ -57,4 +57,7 @@ def admin():
     return render_template('admin.html', bookings=bookings)
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render sets PORT env var
+    app.run(host='0.0.0.0', port=port) 
     app.run(debug=True)
